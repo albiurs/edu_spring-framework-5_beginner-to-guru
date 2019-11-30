db92fb9f9ad884ef1adc8bb35ec56770269debe7
Urs Albisser <u.albisser@gmail.com>
2019-11-30 14:08:46 +0100

    added dependency chuck-norris-for-actuator and implemented JokeService and JokeServiceImpl

---
+++ 04-60ff_SpringBootJokesApp/src/main/java/guru/springframework/joke/services/JokeServiceImpl.java
@@ -0,0 +1,23 @@
+package guru.springframework.joke.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+	// == fields ==
+	private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+	// == constructors ==
+
+
+	public JokeServiceImpl() {
+		this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+	}
+
+	@Override
+	public String getJoke() {
+		return chuckNorrisQuotes.getRandomQuote();
+	}
+}
